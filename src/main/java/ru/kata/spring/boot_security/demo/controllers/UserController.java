0b62448bb292de3694da75125a061936fb53b012
rasulovtim <timmprogramming@gmail.com>
2024-01-03 15:47:12 +0500

    First commit

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.kata.spring.boot_security.demo.entities.User;
 import ru.kata.spring.boot_security.demo.services.UserServiceImpl;
 
+import java.security.Principal;
+
 
 @Controller
 public class UserController {
@@ -17,8 +19,15 @@ public class UserController {
     }
 
     @GetMapping("/user")
-    public String showFormForUpdate(@RequestParam("id")long id, Model model) {
-        User user = userServiceImpl.readUser(id);
+    public String showFormForUpdate(@RequestParam(name = "id", required = false) Long id, Model model, Principal principal) {
+        User user;
+        if (id != null) {
+            user = userServiceImpl.readUser(id);
+        } else {
+            String username = principal.getName();
+
+            user = userServiceImpl.findByUsername(username);
+        }
         model.addAttribute("user", user);
         return "user";
     }
